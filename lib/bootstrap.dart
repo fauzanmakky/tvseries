344229e603527fd7eb9cda47f9171bf842d1f5c6
Fauzan <mohammad.fauzan.majoo@gmail.com>
2026-06-21 14:06:43 +0700

    add firebase crashlytic

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,9 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tvseries/app.dart';
 import 'package:tvseries/di/injection.dart';
+import 'package:tvseries/firebase_options.dart';
 
 Future<void> bootstrapApp() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   await configureDependencies();
   runApp(const MyApp());
 }
